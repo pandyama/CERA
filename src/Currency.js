0feba99e77991e513c82560ea0eff667f01c778e
Meet Pandya <pandyama2@gmail.com>
2021-12-30 11:43:20 -0700

    added ability to convert base amount to targeted currency amount

--- src/Currency.js
+++ src/Currency.js
@@ -3,6 +3,12 @@ import axios from "axios";
 
 export default function Currency() {
   const [currency, setCurrency] = useState([]);
+  const [baseCurrency, setBaseCurrency] = useState();
+  const [targetCurrency, setTargetCurrency] = useState();
+  const [baseAmount, setBaseAmount] = useState("");
+  const [targetAmount, setTargetAmount] = useState("");
+  const [convertedAmount, setConvertedAmount] = useState("");
+
   async function getCurrencies() {
     //   const getCurrencies = () => {
     await axios
@@ -22,6 +28,40 @@ export default function Currency() {
   useEffect(() => {
     getCurrencies();
   }, []);
+
+  async function convert() {
+    console.log(baseCurrency);
+    console.log(targetCurrency);
+    await axios
+      .get(
+        `https://v6.exchangerate-api.com/v6/0a0fe73c79cc22a52d81823a/pair/${baseCurrency}/${targetCurrency}`
+      )
+      .then((res) => {
+        console.log(res.data.conversion_rate);
+        var rate = Number(res.data.conversion_rate);
+        setConvertedAmount((baseAmount*rate).toFixed(2));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function bCurrency(e) {
+    setBaseCurrency(e.target.value);
+    console.log(baseCurrency);
+  }
+  function cCurrency(e) {
+    setTargetCurrency(e.target.value);
+    console.log(targetCurrency);
+  }
+  function bAmount(e) {
+    setBaseAmount(e.target.value);
+    console.log(baseAmount);
+  }
+  function tAmount(e) {
+    setTargetAmount(e.target.value);
+    console.log(targetAmount);
+  }
 
   return (
     <div>
@@ -44,8 +84,9 @@ export default function Currency() {
           placeholder="Enter Amount"
           type="Number"
           className="sInput"
+          onChange={bAmount.bind(this)}
         ></input>
-        <select name="cars" className="inputOptions">
+        <select className="inputOptions" onChange={bCurrency.bind(this)}>
           <option>Pick a Currency</option>
           {currency.map((c, i) => (
             <option value={c} key={i}>
@@ -55,15 +96,23 @@ export default function Currency() {
         </select>
       </div>
       <div className="converted">
-        <input placeholder="Enter Amount" className="sOutput"></input>
-        <select name="cars" className="convertOptions">
-        <option>Pick a Currency</option>
+        <input
+          placeholder="Enter Amount"
+          className="sOutput"
+          onChange={tAmount.bind(this)}
+          value={convertedAmount}
+        />
+        <select className="convertOptions" onChange={cCurrency.bind(this)}>
+          <option>Pick a Currency</option>
           {currency.map((c, i) => (
             <option value={c} key={i}>
               {c}
             </option>
           ))}
         </select>
+      </div>
+      <div className="convert">
+        <button className="convertBTN" onClick={convert}>CONVERT</button>
       </div>
       {/* <div class="area"> */}
       <ul className="circles">
